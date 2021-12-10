f2edb281113f314b4bad26927abd2c801c440de5
maru127 <kj001117@gmail.com>
2021-12-10 21:19:59 +0900

    mask tlq

---
+++ Mask/Mask/main.c
@@ -0,0 +1,18 @@
+/*
+ * Mask.c
+ *
+ * Created: 2021-12-09 오후 11:55:10
+ * Author : JongHun
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
